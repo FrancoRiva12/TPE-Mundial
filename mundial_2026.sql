66ed6be2c0f9747e528f011759ba549920b9cb3a
FrancoRiva12 <francoriva72@gmail.com>
2026-04-16 09:39:16 -0300

    Agregado SQL

---
+++ mundial_2026.sql
@@ -0,0 +1,158 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-04-2026 a las 14:38:10
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `mundial 2026`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estadio`
+--
+
+CREATE TABLE `estadio` (
+  `id_estadio` int(11) NOT NULL,
+  `nombre` varchar(150) NOT NULL,
+  `ciudad` varchar(100) DEFAULT NULL,
+  `capacidad` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grupo`
+--
+
+CREATE TABLE `grupo` (
+  `id_grupo` int(11) NOT NULL,
+  `nombre` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `partido`
+--
+
+CREATE TABLE `partido` (
+  `id_partido` int(11) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `id_estadio` int(11) DEFAULT NULL,
+  `id_local` int(11) DEFAULT NULL,
+  `id_visitante` int(11) DEFAULT NULL,
+  `goles_local` int(11) DEFAULT 0,
+  `goles_visitante` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `seleccion`
+--
+
+CREATE TABLE `seleccion` (
+  `id_seleccion` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `pais` varchar(100) NOT NULL,
+  `id_grupo` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `estadio`
+--
+ALTER TABLE `estadio`
+  ADD PRIMARY KEY (`id_estadio`);
+
+--
+-- Indices de la tabla `grupo`
+--
+ALTER TABLE `grupo`
+  ADD PRIMARY KEY (`id_grupo`);
+
+--
+-- Indices de la tabla `partido`
+--
+ALTER TABLE `partido`
+  ADD PRIMARY KEY (`id_partido`),
+  ADD KEY `id_estadio` (`id_estadio`),
+  ADD KEY `id_local` (`id_local`),
+  ADD KEY `id_visitante` (`id_visitante`);
+
+--
+-- Indices de la tabla `seleccion`
+--
+ALTER TABLE `seleccion`
+  ADD PRIMARY KEY (`id_seleccion`),
+  ADD KEY `id_grupo` (`id_grupo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `estadio`
+--
+ALTER TABLE `estadio`
+  MODIFY `id_estadio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `grupo`
+--
+ALTER TABLE `grupo`
+  MODIFY `id_grupo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `partido`
+--
+ALTER TABLE `partido`
+  MODIFY `id_partido` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `seleccion`
+--
+ALTER TABLE `seleccion`
+  MODIFY `id_seleccion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `partido`
+--
+ALTER TABLE `partido`
+  ADD CONSTRAINT `partido_ibfk_1` FOREIGN KEY (`id_estadio`) REFERENCES `estadio` (`id_estadio`),
+  ADD CONSTRAINT `partido_ibfk_2` FOREIGN KEY (`id_local`) REFERENCES `seleccion` (`id_seleccion`),
+  ADD CONSTRAINT `partido_ibfk_3` FOREIGN KEY (`id_visitante`) REFERENCES `seleccion` (`id_seleccion`);
+
+--
+-- Filtros para la tabla `seleccion`
+--
+ALTER TABLE `seleccion`
+  ADD CONSTRAINT `seleccion_ibfk_1` FOREIGN KEY (`id_grupo`) REFERENCES `grupo` (`id_grupo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
